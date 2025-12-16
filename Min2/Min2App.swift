333c2f56bddbd962f0c7164ea4457e048dde2b13
Lavender <susie204@naver.com>
2025-12-17 05:30:16 +0900

    Initial Commit

---
+++ Min2/Min2App.swift
@@ -0,0 +1,17 @@
+//
+//  Min2App.swift
+//  Min2
+//
+//  Created by 이승현 on 12/17/25.
+//
+//
+import SwiftUI
+
+@main
+struct Min2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
